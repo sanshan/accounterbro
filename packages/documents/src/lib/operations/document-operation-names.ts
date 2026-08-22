e03c5340fe574e84dabb99452483a6e5f31ab53d
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-22 19:49:32 +0200

    feat(documents): add operation names

---
+++ packages/documents/src/lib/operations/document-operation-names.ts
@@ -0,0 +1,2 @@
+export const PREPARE_DOCUMENT_REGISTRATION_OPERATION = 'documents.prepare-registration' as const;
+export const FINISH_DOCUMENT_REGISTRATION_OPERATION = 'documents.finish-registration' as const;
