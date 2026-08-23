385dd5dfca102a27d1cba533b158597cf09b5c98
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-23 10:25:09 +0200

    feat(core): export core identities

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,6 @@
-export * from './lib/core.js';
+export { documentName } from './lib/document/name.js';
+export type { DocumentId } from './lib/document/document-id.js';
+export type { DocumentReference } from './lib/document/document-reference.js';
+export { tenantName } from './lib/tenant/name.js';
+export type { TenantId } from './lib/tenant/tenant-id.js';
+export type { TenantReference } from './lib/tenant/tenant-reference.js';
