e98e2c47b7a5572f3fa81a8947d41c5c74967c6e
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-23 10:24:56 +0200

    feat(core): add tenant reference

---
+++ packages/core/src/lib/tenant/tenant-reference.ts
@@ -0,0 +1,6 @@
+import type { TenantReference as EdpTenantReference } from '@event-driven-platform/tenant-reference';
+
+import { tenantName } from './name.js';
+import type { TenantId } from './tenant-id.js';
+
+export type TenantReference = EdpTenantReference<typeof tenantName, TenantId>;
