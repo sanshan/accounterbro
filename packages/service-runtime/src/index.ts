9d870089eb1afbd4ff8e3e0a715dbb30014dc923
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-24 12:46:22 +0200

    feat(service-runtime): export Read handler resolver

--- packages/service-runtime/src/index.ts
+++ packages/service-runtime/src/index.ts
@@ -1,3 +1,6 @@
 export type { OperationHandlerBinding } from './lib/operation-handler-resolver/operation-handler-binding.js';
 export { MapOperationHandlerResolver } from './lib/operation-handler-resolver/map-operation-handler-resolver.js';
 export { OperationHandlerResolver } from './lib/operation-handler-resolver/operation-handler-resolver.js';
+export type { ReadHandlerBinding } from './lib/read-handler-resolver/read-handler-binding.js';
+export { MapReadHandlerResolver } from './lib/read-handler-resolver/map-read-handler-resolver.js';
+export { ReadHandlerResolver } from './lib/read-handler-resolver/read-handler-resolver.js';
