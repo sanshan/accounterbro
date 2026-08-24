b608ebc58a2556b14ddc8705b9549376957de8ba
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-24 15:23:54 +0200

    feat(documents-service): compose service runtime modules

--- apps/services/documents/src/app/infrastructure/infrastructure.module.ts
+++ apps/services/documents/src/app/infrastructure/infrastructure.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 
 import { DocumentsConfigModule } from './config/documents-config.module';
+import { DocumentsExecutionModule } from './execution/documents-execution.module';
 import { DocumentsTypeormModule } from './persistence/typeorm/documents-typeorm.module';
+import { DocumentsReadModule } from './read/documents-read.module';
 
 @Module({
-    imports: [DocumentsConfigModule, DocumentsTypeormModule],
-    exports: [DocumentsConfigModule, DocumentsTypeormModule],
+    imports: [
+        DocumentsConfigModule,
+        DocumentsTypeormModule,
+        DocumentsExecutionModule,
+        DocumentsReadModule,
+    ],
+    exports: [
+        DocumentsConfigModule,
+        DocumentsTypeormModule,
+        DocumentsExecutionModule,
+        DocumentsReadModule,
+    ],
 })
 export class InfrastructureModule {}
