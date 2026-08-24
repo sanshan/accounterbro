f951bab045ab8031175208d27521eefedf61fb16
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-24 07:37:13 +0200

    feat(service-runtime): export resolver composition

---
+++ packages/service-runtime/src/index.ts
@@ -0,0 +1,3 @@
+export type { OperationHandlerBinding } from './lib/operation-handler-resolver/operation-handler-binding.js';
+export { MapOperationHandlerResolver } from './lib/operation-handler-resolver/map-operation-handler-resolver.js';
+export { OperationHandlerResolver } from './lib/operation-handler-resolver/operation-handler-resolver.js';
