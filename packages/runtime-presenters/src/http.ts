00ffee91faaab458269c33546b083411ae015d97
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-25 22:57:13 +0200

    feat(runtime-presenters): export request identity middleware

--- packages/runtime-presenters/src/http.ts
+++ packages/runtime-presenters/src/http.ts
@@ -1,3 +1,5 @@
 export { Actor } from './lib/http/decorators/actor.decorator.js';
 export { Tenant } from './lib/http/decorators/tenant.decorator.js';
+export { HTTP_REQUEST_IDENTITY_HEADERS } from './lib/http/http-request-identity.headers.js';
+export { HttpRequestIdentityMiddleware } from './lib/http/http-request-identity.middleware.js';
 export type { HttpPresenterRequest } from './lib/http/http-presenter-request.js';
