7e2fc8da07c46c47377c2a521f743ab0803c485d
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-29 17:13:50 +0200

    fix(documents): update use case import paths

--- apps/services/documents/src/app/presenters/http/documents/mappers/register-document.response.mapper.ts
+++ apps/services/documents/src/app/presenters/http/documents/mappers/register-document.response.mapper.ts
@@ -1,4 +1,4 @@
-import type { RegisterDocumentResult } from '../../../../application/use-cases/register-document.use-case';
+import type { RegisterDocumentResult } from '../../../../application/use-cases/register-document/register-document.use-case';
 import type { RegisterDocumentResponseDto } from '../dto/register-document.response.dto';
 
 export function mapRegisterDocumentResponse(
