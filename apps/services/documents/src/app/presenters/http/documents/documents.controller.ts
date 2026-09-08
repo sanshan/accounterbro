8790133c22f03b90ed888ed064c88aa43ee675bd
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-09-08 14:59:05 +0200

    test(review): add PRR-006 boundary case

--- apps/services/documents/src/app/presenters/http/documents/documents.controller.ts
+++ apps/services/documents/src/app/presenters/http/documents/documents.controller.ts
@@ -71,7 +71,7 @@ export class DocumentsController {
     @Post()
     @UseInterceptors(FileInterceptor('file'))
     public async registerDocument(
-        @UploadedFile()
+        @UploadedFile(new ParseFilePipeBuilder().build({ fileIsRequired: true }))
         file: UploadedDocumentFile,
         @Actor() actor: ActorValue,
         @Tenant() tenant: TenantReference,
