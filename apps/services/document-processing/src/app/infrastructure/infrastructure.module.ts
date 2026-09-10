b66c4a28626aaedf9fc409d2cbf48ff36f810595
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-09-10 18:24:03 +0200

    feat: expose document processing capabilities

--- apps/services/document-processing/src/app/infrastructure/infrastructure.module.ts
+++ apps/services/document-processing/src/app/infrastructure/infrastructure.module.ts
@@ -3,7 +3,9 @@ import { RuntimeExecutionsModule } from '@accounterbro/runtime-executions/nest';
 import { Module } from '@nestjs/common';
 
 import { DocumentProcessingConfigModule } from './config/document-processing-config.module';
+import { DocumentProcessingExtractionModule } from './extraction/document-processing-extraction.module';
 import { DocumentProcessingTypeormModule } from './persistence/typeorm/document-processing-typeorm.module';
+import { DocumentProcessingObjectStorageModule } from './storage/document-processing-object-storage.module';
 
 const documentProcessingRuntimeModule = RuntimeExecutionsModule.register([documentProcessing]);
 
@@ -12,11 +14,15 @@ const documentProcessingRuntimeModule = RuntimeExecutionsModule.register([docume
         DocumentProcessingConfigModule,
         DocumentProcessingTypeormModule,
         documentProcessingRuntimeModule,
+        DocumentProcessingObjectStorageModule,
+        DocumentProcessingExtractionModule,
     ],
     exports: [
         DocumentProcessingConfigModule,
         DocumentProcessingTypeormModule,
         documentProcessingRuntimeModule,
+        DocumentProcessingObjectStorageModule,
+        DocumentProcessingExtractionModule,
     ],
 })
 export class InfrastructureModule {}
