0b212ec862e0888665f85f48fbf60c0c96edc827
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-09-10 16:44:29 +0200

    feat: add object storage read contract

--- packages/object-storage/src/lib/ports/object-storage.ts
+++ packages/object-storage/src/lib/ports/object-storage.ts
@@ -7,6 +7,16 @@ export interface ObjectStoragePutResult {
     readonly reference: string;
 }
 
+export interface ObjectStorageGetRequest {
+    readonly reference: string;
+}
+
+export interface ObjectStorageGetResult {
+    readonly content: Uint8Array;
+}
+
 export abstract class ObjectStorage {
     public abstract put(request: ObjectStoragePutRequest): Promise<ObjectStoragePutResult>;
+
+    public abstract get(request: ObjectStorageGetRequest): Promise<ObjectStorageGetResult>;
 }
