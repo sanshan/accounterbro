8be63337f8d898a32032abeed1a2636eda0a8601
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-09-10 16:44:52 +0200

    feat: export object storage read contracts

--- packages/object-storage/src/index.ts
+++ packages/object-storage/src/index.ts
@@ -1,5 +1,7 @@
 export { ObjectStorage } from './lib/ports/object-storage.js';
 export type {
+    ObjectStorageGetRequest,
+    ObjectStorageGetResult,
     ObjectStoragePutRequest,
     ObjectStoragePutResult,
 } from './lib/ports/object-storage.js';
