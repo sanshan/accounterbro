d1c03764248ea95cbce980d71ae222b147ae1b6d
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-09-10 19:33:10 +0200

    feat: register Process Document use case

--- apps/services/document-processing/src/app/application/application.module.ts
+++ apps/services/document-processing/src/app/application/application.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { InfrastructureModule } from '../infrastructure/infrastructure.module';
 import { GetDocumentProcessingUseCase } from './use-cases/get-document-processing/get-document-processing.use-case';
+import { ProcessDocumentUseCase } from './use-cases/process-document/process-document.use-case';
 
 @Module({
     imports: [InfrastructureModule],
-    providers: [GetDocumentProcessingUseCase],
-    exports: [InfrastructureModule, GetDocumentProcessingUseCase],
+    providers: [GetDocumentProcessingUseCase, ProcessDocumentUseCase],
+    exports: [InfrastructureModule, GetDocumentProcessingUseCase, ProcessDocumentUseCase],
 })
 export class ApplicationModule {}
